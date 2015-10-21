cc747e17f1a61d612a0d387af29be855d6e079c3
xeng <habnpam@gmail.com>
2015-10-20 23:08:18 -0700

    2nd commit

---
+++ android/src/com/redditgdx/vang/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.redditgdx.vang.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.redditgdx.vang.RedditGDX;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RedditGDX(), config);
+	}
+}
---
+++ html/src/com/redditgdx/vang/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.redditgdx.vang.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.redditgdx.vang.RedditGDX;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new RedditGDX();
+        }
+}
