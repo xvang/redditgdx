caaa089ba84723ae928c29eba2ec1fe0a76f3ceb
xeng <habnpam@gmail.com>
2015-10-27 02:11:15 -0700

    ...

---
+++ core/src/com/redditgdx/vang/main/AndroidDrawable.java
@@ -0,0 +1,13 @@
+package com.redditgdx.vang.main;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by xeng on 10/27/15.
+ */
+public interface AndroidDrawable {
+
+    public Drawable doDrawable();
+
+
+}
